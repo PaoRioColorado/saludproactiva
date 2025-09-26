8f4b4e998b405b663f13b130c722b608b19cf446
Paola Dueña <paoladf.it@gmail.com>
2025-09-26 14:44:46 -0300

    Actualizar saludproactiva.sql con últimos cambios

--- saludproactiva.sql
+++ saludproactiva.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 04-09-2025 a las 21:00:18
+-- Tiempo de generación: 26-09-2025 a las 19:36:50
 -- Versión del servidor: 10.4.32-MariaDB
--- Versión de PHP: 8.2.12
+-- Versión de PHP: 8.1.25
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -26,236 +26,104 @@ SET time_zone = "+00:00";
 --
 -- Estructura de tabla para la tabla `alertas`
 --
+-- Error leyendo la estructura de la tabla saludproactiva.alertas: #1932 - Table &#039;saludproactiva.alertas&#039; doesn&#039;t exist in engine
+-- Error leyendo datos de la tabla saludproactiva.alertas: #1064 - Algo está equivocado en su sintax cerca &#039;FROM `saludproactiva`.`alertas`&#039; en la linea 1
 
-CREATE TABLE `alertas` (
-  `id` int(11) NOT NULL,
-  `paciente_id` int(11) NOT NULL,
-  `estudio_id` int(11) DEFAULT NULL,
-  `mensaje` text DEFAULT NULL,
-  `fecha_envio` datetime DEFAULT NULL,
-  `enviado` tinyint(1) DEFAULT 0
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `controles`
+--
+-- Error leyendo la estructura de la tabla saludproactiva.controles: #1932 - Table &#039;saludproactiva.controles&#039; doesn&#039;t exist in engine
+-- Error leyendo datos de la tabla saludproactiva.controles: #1064 - Algo está equivocado en su sintax cerca &#039;FROM `saludproactiva`.`controles`&#039; en la linea 1
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `enfermedades`
+--
+-- Error leyendo la estructura de la tabla saludproactiva.enfermedades: #1932 - Table &#039;saludproactiva.enfermedades&#039; doesn&#039;t exist in engine
+-- Error leyendo datos de la tabla saludproactiva.enfermedades: #1064 - Algo está equivocado en su sintax cerca &#039;FROM `saludproactiva`.`enfermedades`&#039; en la linea 1
 
 -- --------------------------------------------------------
 
 --
 -- Estructura de tabla para la tabla `estudios`
 --
+-- Error leyendo la estructura de la tabla saludproactiva.estudios: #1932 - Table &#039;saludproactiva.estudios&#039; doesn&#039;t exist in engine
+-- Error leyendo datos de la tabla saludproactiva.estudios: #1064 - Algo está equivocado en su sintax cerca &#039;FROM `saludproactiva`.`estudios`&#039; en la linea 1
 
-CREATE TABLE `estudios` (
-  `id` int(11) NOT NULL,
-  `paciente_id` int(11) NOT NULL,
-  `tipo` varchar(100) DEFAULT NULL,
-  `fecha` date DEFAULT NULL,
-  `frecuencia` int(11) DEFAULT NULL,
-  `notas` text DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `estudios_pendientes`
+--
+-- Error leyendo la estructura de la tabla saludproactiva.estudios_pendientes: #1932 - Table &#039;saludproactiva.estudios_pendientes&#039; doesn&#039;t exist in engine
+-- Error leyendo datos de la tabla saludproactiva.estudios_pendientes: #1064 - Algo está equivocado en su sintax cerca &#039;FROM `saludproactiva`.`estudios_pendientes`&#039; en la linea 1
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `medicacion`
+--
+-- Error leyendo la estructura de la tabla saludproactiva.medicacion: #1932 - Table &#039;saludproactiva.medicacion&#039; doesn&#039;t exist in engine
+-- Error leyendo datos de la tabla saludproactiva.medicacion: #1064 - Algo está equivocado en su sintax cerca &#039;FROM `saludproactiva`.`medicacion`&#039; en la linea 1
 
 -- --------------------------------------------------------
 
 --
 -- Estructura de tabla para la tabla `medico_registrado`
 --
+-- Error leyendo la estructura de la tabla saludproactiva.medico_registrado: #1932 - Table &#039;saludproactiva.medico_registrado&#039; doesn&#039;t exist in engine
+-- Error leyendo datos de la tabla saludproactiva.medico_registrado: #1064 - Algo está equivocado en su sintax cerca &#039;FROM `saludproactiva`.`medico_registrado`&#039; en la linea 1
 
-CREATE TABLE `medico_registrado` (
-  `id` int(11) NOT NULL,
-  `nombre` varchar(100) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `creado_en` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+-- --------------------------------------------------------
 
 --
--- Volcado de datos para la tabla `medico_registrado`
+-- Estructura de tabla para la tabla `notas`
 --
+-- Error leyendo la estructura de la tabla saludproactiva.notas: #1932 - Table &#039;saludproactiva.notas&#039; doesn&#039;t exist in engine
+-- Error leyendo datos de la tabla saludproactiva.notas: #1064 - Algo está equivocado en su sintax cerca &#039;FROM `saludproactiva`.`notas`&#039; en la linea 1
 
-INSERT INTO `medico_registrado` (`id`, `nombre`, `email`, `password`, `creado_en`) VALUES
-(1, 'Rene Favaloro', 'doctor@correo.com', '$2y$10$34ILQV1OkgwLeaETd061vuiZRzOlXBftkkHmZrIXH5.THUe25jaH2', '2025-08-26 19:28:43');
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `notas_medicas`
+--
+-- Error leyendo la estructura de la tabla saludproactiva.notas_medicas: #1932 - Table &#039;saludproactiva.notas_medicas&#039; doesn&#039;t exist in engine
+-- Error leyendo datos de la tabla saludproactiva.notas_medicas: #1064 - Algo está equivocado en su sintax cerca &#039;FROM `saludproactiva`.`notas_medicas`&#039; en la linea 1
 
 -- --------------------------------------------------------
 
 --
 -- Estructura de tabla para la tabla `pacientes`
 --
+-- Error leyendo la estructura de la tabla saludproactiva.pacientes: #1932 - Table &#039;saludproactiva.pacientes&#039; doesn&#039;t exist in engine
+-- Error leyendo datos de la tabla saludproactiva.pacientes: #1064 - Algo está equivocado en su sintax cerca &#039;FROM `saludproactiva`.`pacientes`&#039; en la linea 1
 
-CREATE TABLE `pacientes` (
-  `id` int(11) NOT NULL,
-  `fecha_nacimiento` date DEFAULT NULL,
-  `enfermedades` text DEFAULT NULL,
-  `apellido` varchar(100) NOT NULL DEFAULT '',
-  `nombre` varchar(100) NOT NULL DEFAULT '',
-  `dni` varchar(20) NOT NULL DEFAULT '',
-  `estado` varchar(20) NOT NULL DEFAULT '',
-  `edad` int(11) DEFAULT NULL,
-  `grupo_sanguineo` varchar(5) NOT NULL DEFAULT '',
-  `telefono` varchar(20) NOT NULL DEFAULT '',
-  `movil` varchar(20) NOT NULL DEFAULT '',
-  `email` varchar(100) NOT NULL DEFAULT '',
-  `direccion` varchar(200) NOT NULL DEFAULT '',
-  `obra_social` varchar(100) DEFAULT NULL,
-  `num_afiliado` varchar(50) NOT NULL,
-  `historia_clinica` varchar(50) NOT NULL DEFAULT ''
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+-- --------------------------------------------------------
 
 --
--- Volcado de datos para la tabla `pacientes`
+-- Estructura de tabla para la tabla `pacientes_backup`
 --
-
-INSERT INTO `pacientes` (`id`, `fecha_nacimiento`, `enfermedades`, `apellido`, `nombre`, `dni`, `estado`, `edad`, `grupo_sanguineo`, `telefono`, `movil`, `email`, `direccion`, `obra_social`, `num_afiliado`, `historia_clinica`) VALUES
-(5, '1954-05-14', 'Hipertensión, Diabetes', 'Dali', 'Salvador', '9123456', 'Alta', 0, '0 +', '', '2915904189', 'dali@gmail.com', 'España 11', 'OSDE', '455', ''),
-(7, '1980-10-25', 'Hipertensión', 'Picasso', 'Pablo', '10123456', 'Alta', 44, '', '', '2914123456', 'picasso@mail.com', 'Malaga 1881', 'MEDIFE', '918', ''),
-(8, '1980-12-18', 'Hipotiroidismo', 'Aguilera', 'Cristina', '20123456', 'Alta', 44, '', '2914123457', '', 'cristina_ag@email.com', 'Staten Island 44', 'Galeno', '45000', '');
+-- Error leyendo la estructura de la tabla saludproactiva.pacientes_backup: #1932 - Table &#039;saludproactiva.pacientes_backup&#039; doesn&#039;t exist in engine
+-- Error leyendo datos de la tabla saludproactiva.pacientes_backup: #1064 - Algo está equivocado en su sintax cerca &#039;FROM `saludproactiva`.`pacientes_backup`&#039; en la linea 1
 
 -- --------------------------------------------------------
 
 --
 -- Estructura de tabla para la tabla `turnos`
 --
-
-CREATE TABLE `turnos` (
-  `id` int(11) NOT NULL,
-  `paciente_id` int(11) NOT NULL,
-  `medico_id` int(11) NOT NULL,
-  `fecha` datetime DEFAULT NULL,
-  `motivo` varchar(255) DEFAULT NULL,
-  `notas` text DEFAULT NULL,
-  `creado_en` timestamp NOT NULL DEFAULT current_timestamp(),
-  `estado` enum('pendiente','confirmado','realizado','cancelado') DEFAULT 'pendiente'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Volcado de datos para la tabla `turnos`
---
-
-INSERT INTO `turnos` (`id`, `paciente_id`, `medico_id`, `fecha`, `motivo`, `notas`, `creado_en`, `estado`) VALUES
-(3, 8, 1, '2025-09-06 16:00:00', 'Ecografía', '', '2025-09-04 15:47:59', 'pendiente'),
-(5, 5, 1, '2025-09-27 14:15:00', 'Control', NULL, '2025-09-04 17:28:12', 'pendiente'),
-(6, 7, 1, '2025-10-15 14:15:00', 'Consulta general', NULL, '2025-09-04 17:43:23', 'confirmado');
+-- Error leyendo la estructura de la tabla saludproactiva.turnos: #1932 - Table &#039;saludproactiva.turnos&#039; doesn&#039;t exist in engine
+-- Error leyendo datos de la tabla saludproactiva.turnos: #1064 - Algo está equivocado en su sintax cerca &#039;FROM `saludproactiva`.`turnos`&#039; en la linea 1
 
 -- --------------------------------------------------------
 
 --
 -- Estructura de tabla para la tabla `usuarios`
 --
-
-CREATE TABLE `usuarios` (
-  `id` int(11) NOT NULL,
-  `nombre` varchar(100) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `rol` enum('medico','paciente') NOT NULL,
-  `creado_en` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `alertas`
---
-ALTER TABLE `alertas`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `paciente_id` (`paciente_id`),
-  ADD KEY `estudio_id` (`estudio_id`);
-
---
--- Indices de la tabla `estudios`
---
-ALTER TABLE `estudios`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `paciente_id` (`paciente_id`);
-
---
--- Indices de la tabla `medico_registrado`
---
-ALTER TABLE `medico_registrado`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `email` (`email`);
-
---
--- Indices de la tabla `pacientes`
---
-ALTER TABLE `pacientes`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `turnos`
---
-ALTER TABLE `turnos`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `paciente_id` (`paciente_id`),
-  ADD KEY `medico_id` (`medico_id`);
-
---
--- Indices de la tabla `usuarios`
---
-ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `email` (`email`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `alertas`
---
-ALTER TABLE `alertas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `estudios`
---
-ALTER TABLE `estudios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `medico_registrado`
---
-ALTER TABLE `medico_registrado`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT de la tabla `pacientes`
---
-ALTER TABLE `pacientes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
-
---
--- AUTO_INCREMENT de la tabla `turnos`
---
-ALTER TABLE `turnos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-
---
--- AUTO_INCREMENT de la tabla `usuarios`
---
-ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- Restricciones para tablas volcadas
---
-
---
--- Filtros para la tabla `alertas`
---
-ALTER TABLE `alertas`
-  ADD CONSTRAINT `alertas_ibfk_1` FOREIGN KEY (`paciente_id`) REFERENCES `pacientes` (`id`),
-  ADD CONSTRAINT `alertas_ibfk_2` FOREIGN KEY (`estudio_id`) REFERENCES `estudios` (`id`);
-
---
--- Filtros para la tabla `estudios`
---
-ALTER TABLE `estudios`
-  ADD CONSTRAINT `estudios_ibfk_1` FOREIGN KEY (`paciente_id`) REFERENCES `pacientes` (`id`);
-
---
--- Filtros para la tabla `turnos`
---
-ALTER TABLE `turnos`
-  ADD CONSTRAINT `turnos_ibfk_1` FOREIGN KEY (`paciente_id`) REFERENCES `pacientes` (`id`);
+-- Error leyendo la estructura de la tabla saludproactiva.usuarios: #1932 - Table &#039;saludproactiva.usuarios&#039; doesn&#039;t exist in engine
+-- Error leyendo datos de la tabla saludproactiva.usuarios: #1064 - Algo está equivocado en su sintax cerca &#039;FROM `saludproactiva`.`usuarios`&#039; en la linea 1
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
